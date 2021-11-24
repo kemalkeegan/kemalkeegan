269e25fe3bdf14f00a641229a99f0834df896099
Kemal Keegan <kemalkeegann@gmail.com>
2021-11-24 11:47:24 +0700

    test

--- src/com/perulangan/ContohPerulangan.java
+++ src/com/perulangan/ContohPerulangan.java
@@ -2,10 +2,13 @@ package com.perulangan;
 
 public class ContohPerulangan {
 	public static void main(String[] args) {
-		int N = 9;
-		for(int i=1;i<=20;i++) {
-			System.out.printf("%d x %d =%d%n",N,i,N*i);
-		}
+//		int N = 9;
+//	for(int i=1;i<=20;i++) {
+//			System.out.printf("%d x %d =%d%n",N,i,N*i);
+//		}
 		
+		for(int i=1;i<=20;i++) {
+			System.out.println("xxxx");
+		}
 	}
 }
